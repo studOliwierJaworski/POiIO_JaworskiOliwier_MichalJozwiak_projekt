b718321db0bb5b01d02b3d5048e357d382e93176
Oliwier Jaworski <jaworskii3226@gmail.com>
2026-05-05 18:42:42 +0200

    Utworzono okno dla strony głównej

---
+++ AplikacjaHotelowa/Home.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace AplikacjaHotelowa {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Podsumowanie informacji o Home
+	/// </summary>
+	public ref class Home : public System::Windows::Forms::Form
+	{
+	public:
+		Home(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: W tym miejscu dodaj kod konstruktora
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Wyczyœæ wszystkie u¿ywane zasoby.
+		/// </summary>
+		~Home()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Wymagana zmienna projektanta.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Metoda wymagana do obs³ugi projektanta — nie nale¿y modyfikowaæ
+		/// jej zawartoœci w edytorze kodu.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Home
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1193, 735);
+			this->Name = L"Home";
+			this->Text = L"Strona g³ówna";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
